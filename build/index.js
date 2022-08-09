76895231ff92fc345b377d151659b0762b3f138c
jmal <zhushilun084@gmail.com>
2022-08-09 15:34:58 +0800

    :art: 删除无用代码

--- build/index.js
+++ build/index.js
@@ -12,8 +12,8 @@ if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
   const port = 9526
   const publicPath = config.publicPath
 
-  var connect = require('connect')
-  var serveStatic = require('serve-static')
+  let connect = require('connect')
+  let serveStatic = require('serve-static')
   const app = connect()
 
   app.use(
