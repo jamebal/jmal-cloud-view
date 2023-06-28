c7f4d257f63862aa60fe2c2d05b92075aaee847c
jmal <zhushilun084@gmail.com>
2023-06-28 15:12:48 +0800

    Update nginx.conf

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # 使用envsubst命令替换nginx.conf.template文件中的环境变量
-envsubst '$API_BASE_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$API_BASE_URL $API_DRAWIO_URL $API_OFFICE_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # 启动Nginx
 exec "$@"
