a8417e6ce167289e69bd624b79e130f4fe2331b9
jmal <zhushilun084@gmail.com>
2024-05-22 16:47:07 +0800

    chore: update Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
+echo "API_URL: $API_URL"
+echo "API_OFFICE_URL: $API_OFFICE_URL"
 envsubst '${API_URL} ${API_OFFICE_URL}' < /var/www/public/config.js.template > /var/www/public/config.js
+cat /var/www/public/config.js
 exec "$@"
