7d2c8cc4fbf5d3987b951f95d5406a6cb2f068fd
jmal <zhushilun084@gmail.com>
2024-06-12 09:56:38 +0800

    chore: v3

--- docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-envsubst '${API_URL} ${API_OFFICE_URL}' < /var/www/public/config.js.template > /var/www/public/config.js
-exec "$@"
--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  semi: false,
-  trailingComma: 'es6',
-  tabWidth: 2,
-  printWidth: 80,
-  endOfLine: 'lf'
-}
