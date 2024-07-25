00d0d829123321b0c07875153169b3aeb3e9f0b8
jmal <zhushilun084@gmail.com>
2024-07-25 16:41:10 +0800

    perf: 显示文件夹大小的选项

--- src/components/ShowFile/ShowFile.vue
+++ src/components/ShowFile/ShowFile.vue
@@ -2594,7 +2594,7 @@ export default {
           child[1].iconClass = 'menu-empty'
         }
         if (child[2]) {
-          if (localStorage.getItem('showFolderSize') === 'true') {
+          if (localStorage.getItem('showFolderSize') === '1') {
             child[2].iconClass = 'duigou'
           } else {
             child[2].iconClass = 'menu-empty'
@@ -3413,7 +3413,11 @@ export default {
           break
         case 'show-folder-size':
           const showFolderSize = localStorage.getItem('showFolderSize')
-          localStorage.setItem('showFolderSize', showFolderSize ? false : true)
+          if (showFolderSize === '1') {
+            localStorage.setItem('showFolderSize', '0')
+          } else {
+            localStorage.setItem('showFolderSize', '1')
+          }
           this.getFileList()
           break
         case 'orderName':
